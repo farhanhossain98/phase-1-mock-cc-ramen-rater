e05271e52260150da7e8cb1881c7a93a715617ba
farhan hossain <farhanrufon99@gmail.com>
2023-07-19 20:58:41 -0400

    modifies index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // write your code here
 
-//Get the ramen info from the json file to here
 
 //constants
+//These are the constants to keep that I will use later
 const baseUrl = "http://localhost:3000"
 const ramenUrl = baseUrl + "/ramens"
 const newRamenForm = document.getElementById("new-ramen")
@@ -13,18 +13,8 @@ const ramenImg = document.getElementById("ramen-image")
 const ramenName = document.getElementById("ramen-name")
 const restaurantName = document.getElementById("restaurant-name")
     
-
-//have to get all the images and info from the json file to appear onto the screen.
-//When the page loads, request the data from the server to get all the ramen objects
-//Using the 
-
-//Main Code
-renderRamens()
-addSubmitListener()
-
-//get ramen menu
-
-//get ramen from server
+//This is the fetch function athat will render in the ramen from server
+//Remember to render in renderRamen()
 function renderRamens(){
 fetch(ramenUrl)
 .then((r) => r.json())
@@ -32,27 +22,24 @@ fetch(ramenUrl)
     ramens.forEach(renderNewRamen)
 })
 }
-//display image for each ramen
+renderRamens()
+
+
 function renderNewRamen(ramen) {
     const img = document.createElement('img')
     img.src = ramen.image
     img.alt = ramen.name
     ramenMenu.appendChild(img)
 
-    //event listener
-    img.addEventListener('click', (e => {
+    img.addEventListener('click', (e) => {
         displayRamen(ramen)
-    }))
+    })
 
 
 }
-//function renderNewRamen(ramen)
-//create image tag for each ramen
 
+//This is to display ramen info 
 function displayRamen(ramen) {
-//details of ramen elements
-//this is so all the details will be shown 
- //this will show all the inner text of the array from the json file
     ramenImg.src = ramen.image
     ramenImg.alt = ramen.name
     restaurantName.innerText = ramen.restaurant
@@ -61,21 +48,18 @@ function displayRamen(ramen) {
     commentDisplay.innerText = ramen.comment
 
 }
-
+//this will be to add any new ramen 
+//when we add new items remember to add e.preventDefault so page does not refresh
 function addSubmitListener() {
     newRamenForm.addEventListener('submit', (e)=>{
         e.preventDefault()
-
-        //create new ramen object with data 
        const newRamenName =  document.getElementById('new-name').value
        const newRamenRestaurant = document.getElementById('new-restaurant').value
        const newRamentImage = document.getElementById('new-image').value
        const newRamenRating = document.getElementById('new-rating').value
        const newRamenComment = document.getElementById('new-comment').value
-
-       // create new object 
-       const newRamen = {
-        
+       
+const newRamen = {    
     "name": newRamenName,
     "restaurant": newRamenRestaurant,
     "image": newRamentImage,
@@ -83,5 +67,7 @@ function addSubmitListener() {
     "comment": newRamenComment
        }
 renderNewRamen(newRamen)
-    })
+    
+})
 }
+addSubmitListener()
